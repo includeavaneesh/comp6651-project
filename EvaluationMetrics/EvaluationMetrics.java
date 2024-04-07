f9f1fb5631e871ae3bd1973560f5f17f79ee2c24
includeavaneesh <avaneeshkanshi5@gmail.com>
2024-04-07 19:44:38 -0400

    Added eval metrics

--- EvaluationMetrics/EvaluationMetrics.java
+++ EvaluationMetrics/EvaluationMetrics.java
@@ -7,5 +7,20 @@ public class EvaluationMetrics {
         return LCCVertexList.size();
     }
 
-    
+    public int maxDegree(LinkedList<Integer>[] graph, LinkedList<Integer> lccVertices) {
+        int maxDegree = 0;
+
+        // Iterate over vertices in the LCC
+        for (int vertex : lccVertices) {
+            int degree = graph[vertex].size(); // Calculate degree of the vertex
+
+            // Update maxDegree if necessary
+            if (degree > maxDegree) {
+                maxDegree = degree;
+            }
+        }
+
+        return maxDegree;
+    }
+
 }
